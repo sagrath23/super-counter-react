3bbb6ccd2f40402d20eae1077b796394cea6092e
Steven Villegas <Steven.Villegas@openintl.com>
2018-01-19 16:44:33 +0000

    Change in title

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Super Counter</h1>
         </header>
         <p className="App-intro">
           Clicked {count} times!!!.
