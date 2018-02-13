d407434ae2ea16814a94cae1a821fee46e955638
Steven Villegas <sagrath23@gmail.com>
2018-02-13 14:29:30 -0500

    component changed to stateless

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -7,32 +9,27 @@ import * as actions from './actions';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const CounterComponent = ({incrementAction, decrementAction, incrementAsyncAction, decrementAsyncAction, count}) => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to Super Counter</h1>
+    </header>
+    <p className="App-intro">
+      Clicked {count} times!!!.
+    </p>
+    <button onClick={() => incrementAction()}>+</button>
+    <button onClick={() => decrementAction()}>-</button>
+    <button onClick={() => incrementAsyncAction()}>+ (async)</button>
+    <button onClick={() => decrementAsyncAction()}>- (async)</button>
+  </div>);
 
-  render() {
-    const {
-      incrementAction, 
-      decrementAction, 
-      incrementAsyncAction,
-      decrementAsyncAction, 
-      count } = this.props;
-          
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Super Counter</h1>
-        </header>
-        <p className="App-intro">
-          Clicked {count} times!!!.
-        </p>
-        <button onClick={(event) => incrementAction()}>+</button>
-        <button onClick={(event) => decrementAction()}>-</button>
-        <button onClick={(event) => incrementAsyncAction()}>+ (async)</button>
-        <button onClick={(event) => decrementAsyncAction()}>- (async)</button>
-      </div>
-    );
-  }
+CounterComponent.propTypes = {
+  incrementAction: PropTypes.func.isRequired,
+  incrementAsyncAction: PropTypes.func.isRequired,
+  decrementAction: PropTypes.func.isRequired,
+  decrementAsyncAction: PropTypes.func.isRequired,
+  count: PropTypes.number.isRequired
 }
 
 const mapStateToProps = (state) => {
@@ -43,4 +40,4 @@ const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(actions, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(CounterComponent);
